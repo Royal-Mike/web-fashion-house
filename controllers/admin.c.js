8c67954cbc02e730da14e9583848f2bfeae1c789
21127561 <71368328+Royal-Mike@users.noreply.github.com>
2023-12-28 16:36:42 +0700

    admin page view and basic controls

--- controllers/admin.c.js
+++ controllers/admin.c.js
@@ -1,4 +1,12 @@
 const adminM = require('../models/admin.m');
 
 module.exports = {
+    home: async (req, res) => {
+        let theme = req.cookies.theme;
+        let dark = theme === "dark" ? true : false;
+        res.render('admin/home', {
+            title: 'Admin',
+            dark: dark
+        });
+    }
 };
